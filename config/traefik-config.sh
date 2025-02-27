5dc7dc3233933ff28bb4f5c25af91f8e006421fb
TriplEight <pisarevden@gmail.com>
2025-02-27 15:11:37 +0100

    fixes for traefik 5

--- config/traefik-config.sh
+++ config/traefik-config.sh
@@ -9,8 +9,8 @@ echo "Creating directories"
 mkdir -p /etc/portainer/pretalx/traefik/ || error "Failed to create traefik folder!"
 echo "Creating traefik config files"
 
-cp config/traefik.yml /etc/portainer/pretalx/traefik/ || error "Failed to create traefik route config!"
-cp -r config/dynamic/ /etc/portainer/pretalx/traefik/ || error "Failed to create traefik config!"
+# cp config/traefik.yml /etc/portainer/pretalx/traefik/ || error "Failed to create traefik route config!"
+# cp -r config/dynamic/ /etc/portainer/pretalx/traefik/ || error "Failed to create traefik config!"
 touch /etc/portainer/pretalx/traefik/acme.json || error "Failed to create traefik certificate storage!"
 chmod 600 /etc/portainer/pretalx/traefik/acme.json
 
