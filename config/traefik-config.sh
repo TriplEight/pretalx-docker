d4bc5234852bbfb9faaefd64ef21edf600c9cb12
TriplEight <pisarevden@gmail.com>
2025-02-27 12:45:42 +0100

    nginx -> traefik; traefik configs

---
+++ config/traefik-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+echo "Creating directories"
+mkdir -p /etc/portainer/pretalx/traefik/ || error "Failed to create traefik folder!"
+echo "Creating traefik config files"
+
+cp traefik.yml /etc/portainer/pretalx/traefik/traefik.yml || error "Failed to create traefik route config!"
+cp -r dynamic/ /etc/portainer/pretalx/traefik/ || error "Failed to create traefik config!"
+touch /etc/portainer/pretalx/traefik/acme.json || error "Failed to create traefik certificate storage!"
+chmod 600 /etc/portainer/pretalx/traefik/acme.json
+
+echo "Traefik configs are in place. Now add your certificates to /etc/portainer/pretalx/traefik/acme.json"
