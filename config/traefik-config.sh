7eeaeeb472ec0010861f0491f29d1849a043a5cc
TriplEight <pisarevden@gmail.com>
2025-02-27 19:40:52 +0100

    old configs work

--- config/traefik-config.sh
+++ config/traefik-config.sh
@@ -6,11 +6,11 @@ function error {
 }
 
 echo "Creating directories"
-mkdir -p /etc/portainer/pretalx/traefik/ || error "Failed to create traefik folder!"
+mkdir -p /etc/portainer/pretalx/traefik/dynamic || error "Failed to create traefik folder!"
 echo "Creating traefik config files"
 
-# cp config/traefik.yml /etc/portainer/pretalx/traefik/ || error "Failed to create traefik route config!"
-# cp -r config/dynamic/ /etc/portainer/pretalx/traefik/ || error "Failed to create traefik config!"
+cp config/traefik-old.yml /etc/portainer/pretalx/traefik/traefik.yml || error "Failed to create traefik route config!"
+cp config/dynamic/config.yml /etc/portainer/pretalx/traefik/dynamic/config.yml || error "Failed to create traefik config!"
 touch /etc/portainer/pretalx/traefik/acme.json || error "Failed to create traefik certificate storage!"
 chmod 600 /etc/portainer/pretalx/traefik/acme.json
 
