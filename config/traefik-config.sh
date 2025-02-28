bd8ef6566dbf4b195cd9c502921f82c984874e1e
TriplEight <pisarevden@gmail.com>
2025-02-28 16:18:26 +0100

    fallback to manual editing 2

--- config/traefik-config.sh
+++ config/traefik-config.sh
@@ -6,17 +6,19 @@ function error {
 }
 
 echo "Creating directories"
-mkdir -p /etc/portainer/pretalx/traefik || error "Failed to create traefik folder!"
+rm -rf /etc/portainer/pretalx/traefik/configs || error "Failed to remove old traefik folder!"
+mkdir -p /etc/portainer/pretalx/traefik/configs || error "Failed to create traefik folder!"
+
 echo "Creating traefik config files"
 
-cp config/traefik.yml /etc/portainer/pretalx/traefik/traefik.yml || error "Failed to create traefik config!"
-cp config/config.yml /etc/portainer/pretalx/traefik/config.yml || error "Failed to create traefik config!"
-cp config/nginx.conf /etc/portainer/pretalx/traefik/nginx.conf || error "Failed to create nginx config!"
-touch /etc/portainer/pretalx/traefik/acme.json || error "Failed to create traefik certificate storage!"
-chmod 600 /etc/portainer/pretalx/traefik/acme.json
+cp config/traefik.yml /etc/portainer/pretalx/traefik/configs/traefik.yml || error "Failed to create traefik config!"
+cp config/config.yml /etc/portainer/pretalx/traefik/configs/config.yml || error "Failed to create traefik config!"
+cp config/nginx.conf /etc/portainer/pretalx/traefik/configs/nginx.conf || error "Failed to create nginx config!"
+touch /etc/portainer/pretalx/traefik/cert/acme.json || error "Failed to create traefik certificate storage!"
+chmod 600 /etc/portainer/pretalx/traefik/cert/acme.json
 
 echo "Traefik configs are in place. Now"
-echo "- edit /etc/portainer/pretalx/traefik/traefik.yml and substitute variables manually"
-echo "- edit /etc/portainer/pretalx/traefik/config.yml and substitute variables manually"
-echo "- edit /etc/portainer/pretalx/traefik/nginx.conf and substitute variables manually"
-echo "- add your certificates to /etc/portainer/pretalx/traefik/acme.json"
+echo "- edit /etc/portainer/pretalx/traefik/configs/traefik.yml and substitute variables manually"
+echo "- edit /etc/portainer/pretalx/traefik/configs/config.yml and substitute variables manually"
+echo "- edit /etc/portainer/pretalx/traefik/configs/nginx.conf and substitute variables manually"
+echo "- add your certificates to /etc/portainer/pretalx/traefik/cert/acme.json"
